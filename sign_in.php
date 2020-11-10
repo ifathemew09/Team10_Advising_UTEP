14fd365dc6100be9ada95077b6c53df85ef76917
Ruth Trejo <rtrejo9@miners.utep.edu>
2020-11-10 15:22:31 -0700

    sign-in page style changes

--- sign_in.php
+++ sign_in.php
@@ -1,10 +1,49 @@
 <!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <meta http-equiv="X-UA-Compatible" content=""IE="Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" type="text/css" href="style_sheets/mainstyle.css">
+
+    </head>
+
+    <div class="loginLogo">
+
+    </div>
+    <div class="loginTitle">
+        <h4>UTEP Single Sign On</h4>
+    </div>
+
+
+    <div class="w3-container">
+        <form action="" method="post">
+            <input type="text" id="users_username" name="users_username" placeholder="User Name (e.g., domain\name" >
+            <input type="password" id="users_pswd" name="users_pswd" placeholder="Password" >
+
+            <div>
+                <button class="sub-btn sub-btn-medium" type="submit" name="submit">Log in</button>
+            </div>
+
+        </form>
+
+    </div>
+
+
+
+</html>
+
+
+<!--
+
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSS -->
+     CSS
     <link rel="stylesheet" href="style_sheets/singin_style.css"/>
 
     <title>UTEP SIGN IN</title>
@@ -12,21 +51,41 @@
 
 <body>
 
-<!-- NAVIGATION PART WITH LOGO TO THE RIGHT -->
+ NAVIGATION PART WITH LOGO TO THE RIGHT
 <div class="navigation-bar">
     <img src="pictures/utep_banner_logo.PNG">
 </div>
 
-<!-- HEADER WITH TITLE -->
+ HEADER WITH TITLE
 <div class="header">
     <h1>COMPUTER SCIENCE ADVISING</h1>
 </div>
 
-<!-- NAVIGATION BUTTON LINK LIST -->
+ NAGIVATION BAR
 <div class="navigation">
     <ul>
+        <li><a href="sign_in.php">Home</a></li>
     </ul>
+</div>
+
+<h2 style="text-align:center">UTEP Single Sign On</h2>
+<div id="login-container">
+    <div class="column">
+        <form action="" method="post">
+            <div class="container">
+                <label for="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" id="uname" required>
+
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    </div>
 </div>
 
 </body>
 </html>
+
+-->
